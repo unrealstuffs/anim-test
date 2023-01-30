985cbd3b1c6fcba5089531804a905caac17adb10
Alexander Labchuk <lab4uk2014@yandex.ru>
2023-01-30 20:47:47 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Lottie from 'lottie-react'
+import animData from './anim-config.json'
+
+import './App.css'
+
+function App() {
+	return <Lottie animationData={animData} />
+}
+
+export default App
